4f57a4560c83164c429b0523d4597f40eb33b91c
Hajime-san <utd.c.r.d.s.a@gmail.com>
2022-07-23 21:07:31 +0900

    feat: update client

--- app/src/components/InputFile/index.tsx
+++ app/src/components/InputFile/index.tsx
@@ -1,28 +1,23 @@
-import { useEffect } from 'react';
-import { useMutationInternalProcess } from '~/hooks/useInternalProcess';
+import { useCallback, useEffect, useState } from 'react';
 import { useOpen } from '~/hooks/useOpen';
 import { valueOf } from '~/types/util';
+import { listen, emit } from '@tauri-apps/api/event'
 
-type ImageContext = {
+import './style.css';
+
+type FileMeta = {
 	path: string;
 	size: number;
 };
 
-type ConvertResult = {
-	status: 'Success' | 'Failed';
-	elapsed: {
-		nano_sec: number;
-		sec: number;
+type FileContext = {
+	id: number,
+	status: 'Initialized' | 'Pending' | 'Success' | 'Failed' | 'Unsupported';
+	input: FileMeta;
+	output: FileMeta & {
+		elapsed: number
 	};
-	input: ImageContext;
-	output: ImageContext;
-};
-
-const useConvertFiles = () => {
-	return useMutationInternalProcess<Array<ConvertResult>, { filePath: Array<string> }>(
-		'command_covert_to_webp',
-	);
-};
+}
 
 const formatBytes = (bytes: number, decimals = 2) => {
 	if (bytes === 0) return '0 Bytes';
@@ -36,39 +31,64 @@ const formatBytes = (bytes: number, decimals = 2) => {
 	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 };
 
+const useFileList = (
+	openedFiles: valueOf<Pick<ReturnType<typeof useOpen>, 'response'>> ,
+) => {
+	const [ files, setFiles ] = useState<Map<number, FileContext>>(new Map())
+
+	const updateFiles = useCallback((key: number, value: FileContext) => {
+		setFiles(map => new Map(map.set(key, value)));
+	}, [])
+
+	useEffect(() => {
+		if (!openedFiles || openedFiles.length === 0) {
+			return;
+		}
+		emit('emit-file', openedFiles)
+	}, [openedFiles]);
+
+	useEffect(() => {
+        let unlisten: any;
+        const f = async () => {
+          unlisten = await listen<string>('listen-file', event => {
+			try {
+				const data = JSON.parse(event.payload) as FileContext
+				updateFiles(data.id, data)
+			} catch (error) {
+				console.log(error);
+			}
+          });
+        }
+        f();
+
+        return () => {
+          if (unlisten) {
+            unlisten();
+          }
+        }
+    }, [])
+
+	return {
+		files
+	}
+}
+
 const FileList = (
 	props: { openedFiles: valueOf<Pick<ReturnType<typeof useOpen>, 'response'>> },
 ) => {
-	const { openedFiles } = props;
-	const convertedProcess = useConvertFiles();
-
-	useEffect(() => {
-		if (!openedFiles) {
-			return;
-		}
-		convertedProcess.mutate({ filePath: openedFiles as Array<string> });
-	}, [openedFiles]);
+	const { files } = useFileList(props.openedFiles)
 
 	return (
-		<div style={{ overflowY: 'scroll', height: 'calc(100% - (62px + 1rem))' }}>
+		<div style={{ overflowY: 'scroll', height: 'calc(100vh - (1rem * 2) - (62px + 1rem))' }}>
 			<ul
-				style={{
-					position: 'sticky',
-					top: 0,
-					left: 0,
-					display: 'grid',
-					gridTemplateColumns: '2fr 1fr 1fr',
-					columnGap: '1rem',
-					justifyContent: 'space-between',
-				}}
+				className='th'
 			>
 				<li>filename</li>
 				<li>size(kb)</li>
 				<li>optimized size(kb)</li>
 			</ul>
-			{Array.isArray(openedFiles) &&
-				openedFiles.map((item, i) => {
-					const convertedFile = convertedProcess.response?.find((v) => v.input.path === item);
+			{[...files].map(([id, item], i) => {
+					const convertedFile = item.status === 'Success'
 					const bgColor = i % 2 === 0 ? 'rgb(225 224 224)' : '#fafafa';
 					return (
 						<ul
@@ -80,11 +100,14 @@ const FileList = (
 								padding: '0.2rem',
 								backgroundColor: bgColor,
 							}}
-							key={item + String(i)}
+							key={id + String(i)}
 						>
-							<li>{item}</li>
-							<li>{convertedFile && formatBytes(convertedFile?.input.size)}</li>
-							<li>{convertedFile && formatBytes(convertedFile?.output.size)}</li>
+							<li>
+								<span>{item.input.path}</span>
+								{convertedFile && <span>✅</span>}
+							</li>
+							<li>{formatBytes(item.input.size)}</li>
+							<li>{convertedFile && formatBytes(item.output.size)}</li>
 						</ul>
 					);
 				})}
@@ -96,7 +119,7 @@ export const InputFile = () => {
 	const { response, error, openHandler } = useOpen({
 		multiple: true,
 		// filters: [{
-		//     name: '.*',
+		//     name: '*',
 		//     extensions: ['png', 'jpeg']
 		// }]
 	});
