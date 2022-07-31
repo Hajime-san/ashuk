936944964c86516044b6d177c6522103b75604ab
Hajime-san <utd.c.r.d.s.a@gmail.com>
2022-07-31 10:55:11 +0900

    chore: remove hook

--- app/src/hooks/useOpenDialogQuery.ts
+++ app/src/hooks/useOpenDialogQuery.ts
@@ -1,39 +1,6 @@
 import { open } from '@tauri-apps/api/dialog';
-import { useState } from 'react';
 import { ProcessConnectionError } from '~/libs/error/ProcessConnectionError';
-import { PromiseType } from '~/types/util';
 import { QueryKey, useQuery, UseQueryOptions } from '@tanstack/react-query';
-
-export const useOpen = (options?: Parameters<typeof open>[0]) => {
-	const [response, setResponse] = useState<PromiseType<ReturnType<typeof open>>>(null);
-	const [error, setError] = useState<ProcessConnectionError>();
-
-	const openHandler = async () => {
-		try {
-			const result = await open(options);
-			// reset error
-			setError(undefined);
-			setResponse(result);
-		} catch (err) {
-			// reset response
-			setResponse(null);
-			if (err instanceof ProcessConnectionError) {
-				setError(new ProcessConnectionError(err));
-				return;
-			}
-			if (err instanceof Error) {
-				setError(new ProcessConnectionError(err));
-			}
-		}
-	};
-
-	return {
-		response,
-		error,
-		openHandler,
-		isValidating: !response || !error,
-	};
-};
 
 type openParameters = Parameters<typeof open>[0];
 
